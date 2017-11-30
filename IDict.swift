0df942143d99b6fa8fe167bf9987f25cae1b1e0e
mharitha28 <mharitha@pdx.edu>
2017-11-30 10:56:20 -0800

    Add files via upload

--- IDict.swift
+++ IDict.swift
@@ -10,8 +10,10 @@ import Foundation
 protocol ADTDict {
     associatedtype V
     associatedtype dict
-    func empty()-> dict
-    func insert(key:String,value:V)
-    func lookup(key:String)-> V?
-    
+    associatedtype A
+    func empty() -> Self
+    func insert(key:String,value:V) 
+    func lookup(key:String) throws -> V?
+    func fold(acc:A,f: (A,String,V) -> A) -> A
 }
+
