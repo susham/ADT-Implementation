b028e3ed2e5600284c236519576ed679a1e2c488
mharitha28 <mharitha@pdx.edu>
2017-11-30 16:25:33 -0800

    Add files via upload

--- Dict_Impl.swift
+++ Dict_Impl.swift
@@ -8,10 +8,7 @@
 
 import Foundation
 public class Dict<T>: ADTDict {
-    
-    
     typealias V = T
-    typealias dict = [String : V]
     
     private class Node {
         var key:String;
@@ -29,50 +26,66 @@ public class Dict<T>: ADTDict {
     self.e = nil;
     }
     
-    func empty() -> dict{
-        return [:]
-        
-    }
-    
+    //This function inserts the key,value into the dictionary and allows only a unique keys in the dictionary
+    //if a key already exists then its value is updated in the dictionary.
     func insert(key:String,value:V){
-        if(e == nil)
-        {       //LinkedList is empty.
-        self.e=Node(Key:key,value:value,next:nil);
+        
+        if(e == nil) //LinkedList is empty.
+        {
+            self.e=Node(Key:key,value:value,next:nil);
         }
         else
         {
-            if(self.e?.next == nil)
-            {
-                self.e?.next=Node(Key:key,value:value,next:nil);
+            if(lookup(key: key) == nil){
+                //key doesn't exist in the LinkedList
+                if(self.e?.next == nil)
+                {
+                    self.e?.next=Node(Key:key,value:value,next:nil);
+                }
+                else
+                {
+                    var current=self.e;
+                    while(current != nil)
+                    {
+                        if(current?.next == nil)
+                        {
+                            current?.next=Node(Key:key,value:value,next:nil);
+                            current=current?.next?.next
+                            
+                        }
+                        else
+                        {
+                            current=current?.next
+                        }
+                    }
+                }
             }
-            else
-            {
-                var current=self.e;
+            else{
+                //dictionary has a key exists, update the value
+                var current=e;
                 while(current != nil)
                 {
-                    if(current?.next == nil)
+                    if(current?.key == key)
                     {
-                     current?.next=Node(Key:key,value:value,next:nil);
-                     current=current?.next?.next
-                        
+                        current?.value=value;
+                        print("updated value");
+                        break;
                     }
-                    else
-                    {
-                        current=current?.next
-                    }
+                    current=current?.next
                 }
             }
         }
     }
     
     
+    //Searches the dictionary for the key provided and returns its value.
     func lookup(key: String) -> T? {
         var current=e;
         while(current != nil)
         {
             if(current?.key == key)
             {
-             return current?.value
+                return current?.value
             }
             else
             {
@@ -81,5 +94,20 @@ public class Dict<T>: ADTDict {
         }
         return nil
     }
+    
+    //This function takes the initial value as the accumulator and the closure function provided by the client.
+    //function is applied on each dictionary value and update the accumulator and then updated accumulator value is
+    //used for next iteration. At the end, accumulator value is sent to the client.
+    func fold<A>(acc: A,function: (A, String, T) -> (A)) -> A{
+        var p=acc;
+        var current=e;
+        while(current != nil)
+        {
+            p=function(p,(current?.key)!,(current?.value)!);
+            current=current?.next
+        }
+        return p;
+    }
+    
     
 }
